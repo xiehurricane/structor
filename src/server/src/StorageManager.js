27c380baa3db2eda71a95e112664a1b00042bbe3
Alexander Pustovalov <apustovalov@gmail.com>
2015-12-25 17:08:11 +0200

    release 0.2.4

--- src/server/src/StorageManager.js
+++ src/server/src/StorageManager.js
@@ -170,14 +170,14 @@ class StorageManager {
     }
 
     compileProjectResources() {
-        return this.installPackages()
-            .then( () => {
-                let pageForDeskFilePath = this.sm.getProject('pageForDesk.filePath');
-                var nodeModulesPath = this.sm.getProject('nodeModules.dirPath');
-                return this.compiler.stopWatchCompiler().then( () => {
-                    return this.compiler.compile(pageForDeskFilePath, this.sm.getProject('desk.dirPath'), 'bundle.js', nodeModulesPath);
-                });
-            });
+        return this.installPackages();
+            //.then( () => {
+            //    let pageForDeskFilePath = this.sm.getProject('pageForDesk.filePath');
+            //    var nodeModulesPath = this.sm.getProject('nodeModules.dirPath');
+            //    return this.compiler.stopWatchCompiler().then( () => {
+            //        return this.compiler.compile(pageForDeskFilePath, this.sm.getProject('desk.dirPath'), 'bundle.js', nodeModulesPath);
+            //    });
+            //});
     }
 
     watchProjectResources(callback){
